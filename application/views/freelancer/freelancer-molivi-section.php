67b1ecbaa6793a7bf8deb9a7703aaf63f2b11daa
lookuppraveen <30824717+lookuppraveen@users.noreply.github.com>
2021-01-18 18:12:21 +0530

    Update freelancer-molivi-section.php
    Commiting files of Jaydeep at 6PM of 18th Jan

--- application/views/freelancer/freelancer-molivi-section.php
+++ application/views/freelancer/freelancer-molivi-section.php
@@ -4,7 +4,7 @@
   <ul>
     <li class="nav-item <?= ($this->uri->segment(1) == 'freelancer-dashboard') ? 'active' : '' ?>"> <a class="" href="<?= base_url().'freelancer-dashboard' ?>"> New </a> </li>
     <li class="nav-item <?= ($this->uri->segment(2) == 'active') ? 'active' : '' ?>"> <a class="" href="<?= base_url().'job-list/active' ?>"> Active </a> </li>
-    <li class="nav-item <?= ($this->uri->segment(2) == 'late') ? 'active' : '' ?>"> <a class="" href="<?= base_url().'job-list/late' ?>"> Late </a> </li>
+    <?php /*<li class="nav-item <?= ($this->uri->segment(2) == 'late') ? 'active' : '' ?>"> <a class="" href="<?= base_url().'job-list/late' ?>"> Late </a> </li> */ ?>
     <li class="nav-item <?= ($this->uri->segment(2) == 'delivered') ? 'active' : '' ?>"> <a class="" href="<?= base_url().'job-list/delivered' ?>"> Delivered </a> </li>
     <li class="nav-item <?= ($this->uri->segment(2) == 'completed') ? 'active' : '' ?>"> <a class="" href="<?= base_url().'job-list/completed' ?>"> Completed </a></li>
     <li class="nav-item <?= ($this->uri->segment(2) == 'cancelled') ? 'active' : '' ?>"> <a class="" href="<?= base_url().'job-list/cancelled' ?>"> Cancelled </a> </li>
