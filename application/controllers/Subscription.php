4f0edd57a4b5fe090007d6d14ed4c535e534540c
amardeepk3 <60055514+amardeepk3@users.noreply.github.com>
2021-01-29 20:36:33 +0530

    Update Subscription.php

--- application/controllers/Subscription.php
+++ application/controllers/Subscription.php
@@ -6,10 +6,23 @@ class Subscription extends CI_Controller {
         parent::__construct();
 		
 		// Load Stripe library
-        $this->load->library('stripe_lib');
-		
+		$this->load->library('stripe_lib');
+		if(!$this->auth->is_logged()) {
+        	$this->session->set_flashdata('msg', '<div class="alert alert-info text-center">You haven\'t login to the portal. Please login to proceed further.</div>');
+        	redirect('sign-in', 'refresh');
+
+        }
+		// if($this->auth->is_logged()) {
+			
+		// 	if($this->session->userdata('user_type') == 1){
+		// 		redirect('admin/dashboard', 'refresh');
+		// 	}else{
+		// 		redirect('dashboard', 'refresh');
+		// 	}
+        // }
 		// Get user ID from current SESSION
 		$this->userID = isset($_SESSION['user_id'])?$_SESSION['user_id']:1;
+
     }
     
     public function index(){
@@ -17,9 +30,9 @@ class Subscription extends CI_Controller {
 		$content = $this->parser->parse('account/subscription_plan',$parsedata,true);//$this->laccount->sign_up_as_page();
 		$data = array(
 			        'content' => $content,
-			        'title' => display('Payment :: Hire-n-Work'),
+			        'title' => display('Subscription :: Hire-n-Work'),
 		        );
-		$this->template->full_website_html_view($data);	
+		$this->template->full_customer_html_view($data);	
     }
 	
 	public function payment(){
@@ -32,8 +45,8 @@ class Subscription extends CI_Controller {
 		$content = $this->parser->parse('account/subscription_payment',$parsedata,true);//$this->laccount->sign_up_as_page();
 		$data = array(
 			        'content' => $content,
-			        'title' => display('Payment :: Hire-n-Work'),
+			        'title' => display('Subscription :: Hire-n-Work'),
 		        );
-		$this->template->full_website_html_view($data);
+		$this->template->full_customer_html_view($data);
 	}
 }
