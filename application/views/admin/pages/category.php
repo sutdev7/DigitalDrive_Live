6fb27483a05842b134f5828c15a75011d17f79f1
98acjha9 <62154596+98acjha9@users.noreply.github.com>
2021-02-02 20:26:57 +0530

    Change Design
    Done

--- application/views/admin/pages/category.php
+++ application/views/admin/pages/category.php
@@ -1,5 +1,5 @@
 <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
+<div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <div class="content-header">
       <div class="container-fluid">
@@ -27,7 +27,7 @@
 				<div class="card">
 					<div class="card-header">
 					  <h3 class="card-title">List of Skills Category</h3>
-					  <!-- <a class="btn btn-info float-right btn-sm" href="<?= base_url().'admin/category-add'?>"><i class="fa fa-plus"></i> Add</a> -->
+					  <a class="btn btn-info float-right btn-sm" href="<?= base_url().'admin/category_add_skills'?>"><i class="fa fa-plus"></i> Add</a> 
 					</div>
 					<!-- /.card-header -->
 					<div class="card-body"><?php echo $this->session->userdata('msg'); ?>
@@ -36,9 +36,9 @@
 						<tr>
 						  <td width="10%">Sl No.</td>
 						  <th width="30%">Category Name</th>
-						  <th>Sub Category</th>
-						  <th width="10%" align="center">Details Sub Category </th>
-						  <!-- <th width="10%">Action</th> -->
+						  <!-- <th>Sub Category</th> -->
+						 <!--  <th width="10%" align="center">Details Sub Category </th> -->
+						  <th width="10%">Action</th> 
 						</tr>
 						</thead>
 						<tbody>
@@ -49,23 +49,35 @@
 							?>
 							<tr>
 							  <td><?= $count++ ?></td>
-							  <td><?= $row->category_name ?></td>
-							  <td><?php 
-							 $cat_name=$row->category_name;
-							  $subcategorylist = $this->Admimodel->get_sub_category($cat_name);
-							  echo $subcategorylist;
+							  <td><?php  
+							  if($row->parent_id==0){
+							  	echo $row->category_name ;
+								}else{
+									$cat = $row->parent_id;
+									 //$subcategorylist = $this->Admimodel->get_sub_category($cat_name);
+									 $subcategorylist = $this->Admimodel->get_sub_category($cat);
+									echo $row->category_name.'('.$subcategorylist->category_name.')' ;
+								}
 							  ?></td>
+							 <!--  <td><?php 
+							 $cat_name=$row->category_id;
+							  $subcategorylist = $this->Admimodel->get_sub_category($cat_name);
+							  foreach ($subcategorylist as $key => $value) {
+							  	echo $value->category_name.",";
+							  }
+							  ?></td> -->
+							 
 							  <td>
-								<div class="custom-control custom-switch">
-								 <!--  <input type="checkbox" name="status" class="custom-control-input activebtn" data-id="<?=$row->category_id?>" id="customSwitch<?=$row->category_id?>" <?= ($row->status == 1) ? 'checked': '' ?> value="<?= ($row->status == 1) ? '0': '1' ?>" >
-								  <label class="custom-control-label" for="customSwitch<?=$row->area_of_interest_id?>"></label>
-								  <i class="fa ion-android-sync fa-spin" id="wait<?=$row->area_of_interest_id?>" style="display:none;"></i> -->
-								  <a type="button" class="btn btn-info" title="subcategryview" href="<?= base_url().'admin/sub_category_listing/'.$row->category_name ?>">Details Sub Category</a>
+								
+								
+
+								<div class="btn-group">
+									<a type="button" class="btn btn-info" title="Edit" href="<?= base_url().'admin/category_edit_skills/'.$row->category_id ?>"><i class="fa fa-edit"></i></a>
 								</div>
-							  </td>
-							 <!--  <td>
-								<a type="button" class="btn btn-info" title="Edit" href="<?= base_url().'admin/category-edit/'.base64_encode($row->area_of_interest_id) ?>"><i class="fa fa-edit"></i></a>
-							  </td> -->
+								<div class="btn-group">
+									<a type="button" class="btn btn-danger" title="Delete" href="<?= base_url().'admin/delete_catgory/'.$row->category_id ?>" onClick="return confirm('Do you want to delete ?');"><i class="fa fa-trash"></i></a>
+								</div>
+							  </td> 
 							</tr>
 							<?php
 							}
@@ -80,9 +92,9 @@
 						<tr>
 						  <td>Sl No.</td>
 						  <th>Category Name</th>
-						  <th>Sub Category</th>
-						  <th>Details Sub Category</th>
-						  <!-- <th>Action</th> -->
+						  <!-- <th>Sub Category</th> -->
+						  
+						   <th>Action</th> 
 						</tr>
 						</tfoot>
 					  </table>
