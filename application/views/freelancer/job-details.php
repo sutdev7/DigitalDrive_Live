4f6f41458a8c4674c197e428d5c4c6e91d1e1a46
amardeepk3 <60055514+amardeepk3@users.noreply.github.com>
2021-02-05 13:11:25 +0530

    Update job-details.php
    changes in job details

--- application/views/freelancer/job-details.php
+++ application/views/freelancer/job-details.php
@@ -187,7 +187,11 @@
                             <input type="hidden" value="{task_info}{task_id}{/task_info}" name="task_id">
                             <input type="submit" value="Submit Proposal" class="eyeBtn">
                         </form-->
-                        <a href="javascript:void(0);" data-toggle="modal" data-target="#myModal">SUBMIT PROPOSAL</a>
+                       <?php if(trim($proposal_info_is_already) == "yes" ){ ?>
+                        <a href="javascript:void(0);" data-toggle="modal" data-target="#myModalview">VIEW PROPOSAL</a>
+                        <?php }else{ ?>
+                          <a href="javascript:void(0);" data-toggle="modal" data-target="#myModal">SUBMIT PROPOSAL</a>
+                        <?php } ?>
                         <?php
                     }
                 ?>
@@ -312,6 +316,96 @@
 
                 <div class="body">
                     <div class="row cover">
+                      <!-- added by amar -->
+                      
+                      <div class="col-md-12 col-lg-12 col-xl-12">
+
+                      <!-- <h3>Terms</h3> -->
+                      <div class="frmList">
+                        <div class="radiodiv" style="padding-top:0;">
+                          <ul>
+                            <li>
+                              <label class="containerdiv newopen1">Amount / Hr
+                                <input type="radio" name="terms" value="pay_whole_amount" checked="checked">
+                                <span class="checkmark"></span> </label>
+                            </li>
+                            <li>
+                              <label class="containerdiv newopen2">Amount by Milestone
+                                <input type="radio" name="terms" value="pay_by_milestone">
+                                <span class="checkmark"></span> </label>
+                            </li>
+                          </ul>
+                        </div>
+
+                        </div>
+                        </div>
+
+                      <div class="bidding-amt col-md-4 col-lg-4 col-xl-4">
+                            <div class="portal-text">
+                                <h5>Bidding Amount</h5>
+                                <input type="text" name="terms_amount_max" placeholder="$ 0" id="terms_amount_max" required>
+                                <!-- <span>Amount / hr</span> -->
+                            </div>
+                            <div class="portal-text">
+                                <h5>Portal Charges</h5>
+                                <input type="text" placeholder="$ 0" disabled="" id="portal_charges" name="portal_charges">
+                            </div>
+                        </div>
+
+                        <div class="portal-charges col-md-3 col-lg-3 col-xl-3">
+                            <div class="portal-text">
+                                <h5>3rd Party Charges</h5>
+                                <input type="text" placeholder="$ 0" disabled="" name="3rd_party_charges" id="3rd_party_charges">
+                            </div>
+                            <div class="portal-text">
+                                <h5>Earn Amount</h5>
+                                <input type="text" placeholder="$ 0" name="earn_amount" disabled="" id="earn_amount">
+                            </div>
+
+                        </div>
+
+                        <div class="proposal text-center col-md-4 col-lg-4 col-xl-4">
+                            <p>Required Key to submit this proposal 1</p>
+                            <h4>Available Key {connection}</h4>
+                        </div>
+
+                      <div class="col-md-12 col-lg-12 col-xl-12">
+
+                          <div class="opendiv2" style="display:none;">
+                                  <ul>
+                                    <li class="row after-add-more">
+                                    <div class="col-lg-4 col-md-12 col-xs-12">
+                                      <label>Title</label>
+                                      <input type="text" name="milestone_title[]" class="form-control" placeholder="Title">
+                                    </div>
+                                    <!-- <div class="col-lg-4 col-md-12 col-xs-12">
+                                      <label>Hire Date</label>
+                                      <div id="datepicker" class="input-group date milestone_end_date" data-date-format="mm-dd-yyyy">
+                                      <input class="form-control" type="text" name="milestone_end_date[]" value="" />
+                                      <span class="input-group-addon"><i class="fa fa-calendar"></i></span> </div>
+                                    </div> -->
+                                    <div class="col-lg-4 col-md-12 col-xs-12">
+                                      <label>Amount</label>
+                                      <div class="input-group amt"> <span class="input-group-addon"><i class="fa fa-dollar"></i></span>
+                                      <input class="form-control milestone_amount" id="" type="text" name="milestone_agreed_budget[]" value="" placeholder="00">
+                                      </div>
+                                    </div>
+                                    <div class="col-lg-4 col-md-12 col-xs-12">
+                                      <div> 
+                                        <a href="#" class="cancelBtn"><i class="fa fa-times" aria-hidden="true"></i></a>
+                                      </div>
+                                    </div>
+                                    </li>
+                                  
+                                                    
+                                          
+                                    <li class="row" style="padding:40px 0; border:none;"> 
+                                      <a href="#" class="plus_btn" id="addMore"><i class="fa fa-plus"></i> Add Another</a> <span class="optionalSpan"></span> 
+                                    </li>
+                                  </ul>
+                          </div>
+                      </div>
+                      <!-- end by amar -->
                         <div class="col-md-8 col-lg-8 col-xl-8">
                             <div class="cover-letter">
                                 <h4>Cover letter</h4>
@@ -323,10 +417,10 @@
                             <textarea name="cover_letter" id="the-textarea" required maxlength="700" placeholder="Type here" autofocus></textarea>
                         </div>
 
-                        <div class="proposal text-center col-md-4 col-lg-4 col-xl-4">
+                        <!-- <div class="proposal text-center col-md-4 col-lg-4 col-xl-4">
                             <p>Required Key to submit this proposal 1</p>
                             <h4>Available Key {connection}</h4>
-                        </div>
+                        </div> -->
                     </div>
 
                     <div class="row charges">
@@ -348,7 +442,7 @@
                             </div>
                         </div>
 
-                        <div class="bidding-amt col-md-4 col-lg-4 col-xl-4">
+                        <!-- <div class="bidding-amt col-md-4 col-lg-4 col-xl-4">
                             <div class="portal-text">
                                 <h5>Bidding Amount</h5>
                                 <input type="text" name="terms_amount_max" placeholder="$ 0" id="terms_amount_max" required>
@@ -370,7 +464,7 @@
                                 <input type="text" placeholder="$ 0" name="earn_amount" disabled="" id="earn_amount">
                             </div>
 
-                        </div>
+                        </div> -->
                     </div>
                 </div>
 
@@ -395,6 +489,111 @@
         </div>
 
     </div>
+
+<!-- Modal end -->
+
+
+<!-- view proposal Modal -->
+<div id="myModalview" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+            <!-- Modal content-->
+            <form action="<?= base_url().'freelancer/submit_proposal' ?>" method="post" enctype="multipart/form-data">
+            <input type="hidden" name="task_id" value="<?= $task_id ?>">
+            <input type="hidden" name="user_task_id" value="<?= $user_task_id ?>">
+            <div class="modal-content">
+                <div class="header">
+                    <h2>View Job</h2>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+
+                <div class="body">
+                    <div class="row cover">
+
+                      
+          <div class="mbl-table-nine">
+            <div class="mbldiv-scroll">
+              <table class="table">
+                <thead>
+                  <tr>
+                    
+                    <th>Date</th>
+                    <th>Title</th>
+                    <th>Milestone Budget</th>
+                    <!-- <th>Provided By</th> -->
+                    <!-- <th>Action</th> -->
+                  </tr>
+                </thead>
+                <tbody id="dataList">
+                <!-- Display posts list -->
+			            <?php if(!empty($proposal_info)){ foreach($proposal_info as $row){ ?>
+                  <tr>
+                  	<td> <?php  echo date('Y-m-d',strtotime($row["milestone_doc"])); ?></td>
+                    <td><?php  echo $row["milestone_title"]; ?></td>
+                    
+                    <td><?php echo $row["milestone_agreed_budget"]; ?></td>
+                    <!-- <td><?php //echo $row["provided_email"]; ?></td> -->
+                    <!-- <td> -->
+                    <!-- <a data-toggle="tooltip" data-placement="top" title="withdraw">
+                    <i class="fa fa-money" style="font-size:24px;"></i></a>  -->
+                        <!-- <a data-toggle="tooltip" data-placement="top" title="Details" href="<?php //echo base_url() ;?>hired-job-details/<?php echo $row['user_task_id'] ;?>"> -->
+                          <!-- <i class="fa fa-eye" style="font-size:24px;" aria-hidden="true"></i> -->
+                          <!-- </a> -->
+                  <!-- </td> -->
+                    
+                  </tr>
+                  <!-- <tr>
+                    <td><img src="img/cal-img.png" alt=""> 10/12/2019 </td>
+                    <td>UI Design</td>
+                    
+                    <td>$50</td>
+                  </tr> -->
+                    <?php } }else{ ?>
+                    <tr><td  colspan="5"><p>Records not found...</p></td></tr>
+                  <?php } ?>
+                      <!-- Render pagination links -->
+                    <tr>
+                    <td  colspan="5">
+                      <?php // echo ($this->ajax_pagination->create_links() !== "") ? $this->ajax_pagination->create_links():""; ?>
+                      
+                    </td>
+                    </tr>
+                  
+                </tbody>
+              </table>
+              
+            </div>
+            
+          </div>
+
+                    </div>
+
+                </div>
+
+                <div class="line">
+
+                </div>
+
+                <footer class="footer">
+                    <div class="cta">
+                        <!-- <button class="btn submit" type="submit">Submit</button> -->
+                        <button class="btn cancel" data-dismiss="modal">Cancel</button>
+                    </div>
+                </footer>
+            </div>
+        </form>
+            <!-- <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+      <div class="modal-body">
+        <p>Some text in the modal.</p>
+      </div> -->
+        </div>
+
+    </div>
+
+<!-- view proposal Modal end -->
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
  <script src="https://jeremyfagis.github.io/dropify/dist/js/dropify.min.js"></script>
 <script> 
@@ -447,6 +646,25 @@
           $("#3rd_party_percentage").val('2%');
           $("#earn_amount").val(freelancer_amount);
         });
+
+        // $("#addMore").on("click",function(e){ 
+        //   alert('hii');
+        // e.preventDefault();
+        //     var html = $(".after-add-more").first().clone();
+        // var total_element = $(".after-add-more").length;
+        // var total_element = total_element +1;
+        //     $(".after-add-more").last().after(html);
+        // $("#noofmilestone").html(total_element);
+        // });
+
+        // $("body").on("click",".cancelBtn",function(e){ 
+        // e.preventDefault();
+        // var total_element = $(".after-add-more").length;
+        // if(total_element >= 2){
+        //   $(this).parents(".after-add-more").remove();
+        // }
+            
+        // });
     });
 	
     function copyToClipboard(element) {
@@ -457,7 +675,78 @@
         alert('Linked Copied');
         $temp.remove();
     }
-	
-	
+
+        var currentamount = 0;
+        var wholeamount = 0;
+        
+        $("#addMore").on("click",function(e){ 
+          // alert('hii');
+        e.preventDefault();
+            var html = $(".after-add-more").first().clone();
+        var total_element = $(".after-add-more").length;
+        var total_element = total_element +1;
+        
+
+        var firstval = $(".milestone_amount").first().val();
+        var lastval = $(".milestone_amount").last().val();
+
+        // alert("firstval" + firstval);
+        // alert("lastval" + lastval);
+
+
+        wholeamount = $("#terms_amount_max").val();
+        if(wholeamount <= 0){
+          alert("please select the bidding amount");
+        }else{
+
+        var sumtotal = 0;
+
+        // alert(wholeamount);
+        $(".milestone_amount").each(function(){
+          sumtotal = sumtotal + Number($(this).val());
+          // alert("currentval" + $(this).val());;
+        });
+        // alert("sumtotal" + sumtotal);
+        if( sumtotal >= wholeamount ){
+          alert("please reduce milestone amount to add different milestone")
+        }else{
+         $(".milestone_amount").first().val(wholeamount- sumtotal );
+
+          $(".after-add-more").last().after(html);
+          $("#noofmilestone").html(total_element);
+      }
+    }
+            
+        });
+
+        $(".cancelBtn").on("click",function(e){ 
+        e.preventDefault();
+        var total_element = $(".after-add-more").length;
+        alert(total_element);
+
+        if(total_element >= 2){
+          $(this).parents(".after-add-more").remove();
+        }
+            
+        });
+
+        $(".opendiv1").show();
+        $(".opendiv2").hide();
+        $(".newopen1").click(function(){
+          $(".opendiv1").show();
+          $(".opendiv2").hide();
+        });
+        
+        $(".newopen2").click(function(){
+          $(".opendiv2").show();
+          $(".opendiv1").hide();
+        });
+
+
+        $(function (){
+          $('body').on('focus',".milestone_end_date", function(){
+            $(this).datepicker();
+          });
+        });
 </script>	
 <script src="../assets/js/job_page.js"></script>
