2596d03b16c094f820978e732d49235811575a56
amardeepk3 <60055514+amardeepk3@users.noreply.github.com>
2021-02-09 18:50:41 +0530

    Update task-details.php

--- application/views/task/task-details.php
+++ application/views/task/task-details.php
@@ -115,7 +115,7 @@ else{
                                     <h5>Hired</h5>
                                     <em><i class="fa fa-user" aria-hidden="true"></i> {task_freelancer_hire} Hired</em> </span>
                                 <span>
-                                    <h5><a href="javascript:void(0);" data-toggle="modal" data-target="#myModalview">Total Proposals</a></h5>
+                                    <h5>Total Proposals</h5>
                                     <?php
                                     if(count($proposals) > 0) {
                                         ?>
@@ -223,22 +223,22 @@ else{
                                         <div class="PflImgHldrWrpr">
                                             
                                             <div class="card text-center"  style="width: 350px">
- <div class="Imghldr" style="height: 73px;background:url(<?php echo $user_profile_image; ?>) no-repeat center center; background-size:cover;"></div>
- <!-- <img src="" class="Imghldr" style=" margin-right: 47px;"> -->
-    <h5 class="card-title"style="margin-left:-127px;margin-bottom: -6px;margin-top: -50px;"><?php echo ucfirst($fRow->name); ?></h5>
-    <p class="card-text">
-      <h6 style="margin-left:-90px;"><?php echo isset($ss['user_selected_skills'][0]) ? $ss['user_selected_skills'][0]['skill_name'] : ''; ?></h6>
-                                            <h6 style="margin-left:-111px;"><?php echo $fRow->country;?></h6>
-    </p>
-  
-  
-  <div class="" style="display: flex;"> 
-    <span class="plus" style="flex:1;margin-top: 21px;margin-right: 122px;">+ <?php echo $fRow->total_positive_coins; ?> Coins</span>
-                                            <span id="negetive_coin"> <?php echo $fRow->total_negative_coins; ?> Coins</span>
-                                        </div>
-  </div>
- 
-</div>
+                                    <div class="Imghldr" style="height: 73px;background:url(<?php echo $user_profile_image; ?>) no-repeat center center; background-size:cover;"></div>
+                                    <!-- <img src="" class="Imghldr" style=" margin-right: 47px;"> -->
+                                        <h5 class="card-title"style="margin-left:-127px;margin-bottom: -6px;margin-top: -50px;"><?php echo ucfirst($fRow->name); ?></h5>
+                                        <p class="card-text">
+                                        <h6 style="margin-left:-90px;"><?php echo isset($ss['user_selected_skills'][0]) ? $ss['user_selected_skills'][0]['skill_name'] : ''; ?></h6>
+                                                                                <h6 style="margin-left:-111px;"><?php echo $fRow->country;?></h6>
+                                        </p>
+                                    
+                                    
+                                    <div class="" style="display: flex;"> 
+                                        <span class="plus" style="flex:1;margin-top: 21px;margin-right: 122px;">+ <?php echo $fRow->total_positive_coins; ?> Coins</span>
+                                                                                <span id="negetive_coin"> <?php echo $fRow->total_negative_coins; ?> Coins</span>
+                                                                            </div>
+                                    </div>
+                                    
+                                    </div>
                                         <!-- <div class="Txthldr">
                                             <h2></h2>
                                             
@@ -265,112 +265,6 @@ else{
         <!--Changes Abhishek Jha-->
     </section>
 </main>
-
-<!-- view proposal Modal -->
-<div id="myModalview" class="modal fade" role="dialog">
-        <div class="modal-dialog modal-dialog-scrollable">
-
-            <!-- Modal content-->
-            <form action="<?= base_url().'freelancer/submit_proposal' ?>" method="post" enctype="multipart/form-data">
-            <input type="hidden" name="task_id" value="<?= $task_id ?>">
-            <input type="hidden" name="user_task_id" value="<?= $user_task_id ?>">
-            <div class="modal-content">
-                <div class="header">
-                    <h2>View Proposal</h2>
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                </div>
-
-                <div class="body">
-                    <div class="row cover">
-
-                      
-          <div class="mbl-table-nine">
-            <div class="mbldiv-scroll">
-              <table class="table">
-                <thead>
-                  <tr>
-                    
-                    <th>Date</th>
-                    <th>Title</th>
-                    <th>Milestone</th>
-                    <th>send by</th>
-
-
-                    <!-- <th>Provided By</th> -->
-                    <!-- <th>Action</th> -->
-                  </tr>
-                </thead>
-                <tbody id="dataList">
-                <!-- Display posts list -->
-			            <?php if(!empty($proposal_info)){ foreach($proposal_info as $row){ ?>
-                  <tr>
-                  	<td> <?php  echo date('Y-m-d',strtotime($row["doc"])); ?></td>
-                    <td><?php  echo $row["milestone_title"]; ?></td>
-                    
-                    <td><?php echo $row["milestone_agreed_budget"]; ?></td>
-                    <td><?php echo $row["unique_id"]; ?></td>
-                    
-                    <!-- <td><?php //echo $row["provided_email"]; ?></td> -->
-                    <!-- <td> -->
-                    <!-- <a data-toggle="tooltip" data-placement="top" title="withdraw">
-                    <i class="fa fa-money" style="font-size:24px;"></i></a>  -->
-                        <!-- <a data-toggle="tooltip" data-placement="top" title="Details" href="<?php //echo base_url() ;?>hired-job-details/<?php echo $row['user_task_id'] ;?>"> -->
-                          <!-- <i class="fa fa-eye" style="font-size:24px;" aria-hidden="true"></i> -->
-                          <!-- </a> -->
-                  <!-- </td> -->
-                    
-                  </tr>
-                  <!-- <tr>
-                    <td><img src="img/cal-img.png" alt=""> 10/12/2019 </td>
-                    <td>UI Design</td>
-                    
-                    <td>$50</td>
-                  </tr> -->
-                    <?php } }else{ ?>
-                    <tr><td  colspan="5"><p>Records not found...</p></td></tr>
-                  <?php } ?>
-                      <!-- Render pagination links -->
-                    <tr>
-                    <td  colspan="5">
-                      <?php // echo ($this->ajax_pagination->create_links() !== "") ? $this->ajax_pagination->create_links():""; ?>
-                      
-                    </td>
-                    </tr>
-                  
-                </tbody>
-              </table>
-              
-            </div>
-            
-          </div>
-
-                    </div>
-
-                </div>
-
-                <div class="line">
-
-                </div>
-
-                <footer class="footer">
-                    <div class="cta">
-                        <!-- <button class="btn submit" type="submit">Submit</button> -->
-                        <button class="btn cancel" data-dismiss="modal">Cancel</button>
-                    </div>
-                </footer>
-            </div>
-        </form>
-            <!-- <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-      </div>
-      <div class="modal-body">
-        <p>Some text in the modal.</p>
-      </div> -->
-        </div>
-
-    </div>
-
-<!-- view proposal Modal end -->
 
 <style>
 .RvwLists li {
